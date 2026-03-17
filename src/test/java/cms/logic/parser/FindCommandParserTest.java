d0db2d877798a40c3c398065840309440dcc0ae0
derpymoo <zhengyijie183@gmail.com>
2026-03-18 01:33:59 +0800

    Improve path coverage

--- src/test/java/cms/logic/parser/FindCommandParserTest.java
+++ src/test/java/cms/logic/parser/FindCommandParserTest.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.Test;
 
 import cms.logic.commands.FindCommand;
 import cms.model.person.NameContainsKeywordsPredicate;
+import cms.model.person.NusIdContainsKeywordsPredicate;
 
 public class FindCommandParserTest {
 
@@ -29,6 +30,43 @@ public class FindCommandParserTest {
 
         // multiple whitespaces between keywords
         assertParseSuccess(parser, " \n Alice \n \t Bob  \t", expectedFindCommand);
+    }
+
+    @Test
+    public void parse_namePrefix_returnsFindCommand() {
+        FindCommand expectedFindCommand =
+                new FindCommand(new NameContainsKeywordsPredicate(Arrays.asList("Alice", "Bob")));
+        assertParseSuccess(parser, " n/Alice Bob", expectedFindCommand);
+    }
+
+    @Test
+    public void parse_idPrefix_returnsFindCommand() {
+        FindCommand expectedFindCommand =
+                new FindCommand(new NusIdContainsKeywordsPredicate(Arrays.asList("A0234504F")));
+        assertParseSuccess(parser, " id/A0234504F", expectedFindCommand);
+    }
+
+    @Test
+    public void parse_idPrefix_multipleIds_returnsFindCommand() {
+        FindCommand expectedFindCommand =
+                new FindCommand(new NusIdContainsKeywordsPredicate(Arrays.asList("A0234502D", "A0234505G")));
+        assertParseSuccess(parser, " id/A0234502D A0234505G", expectedFindCommand);
+    }
+
+    @Test
+    public void parse_bothPrefixes_throwsParseException() {
+        assertParseFailure(parser, " n/Alice id/A0234504F",
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
+    }
+
+    @Test
+    public void parse_emptyNamePrefix_throwsParseException() {
+        assertParseFailure(parser, " n/   ", String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
+    }
+
+    @Test
+    public void parse_emptyIdPrefix_throwsParseException() {
+        assertParseFailure(parser, " id/  ", String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
     }
 
 }
