4c91c264a90f49246f60347c04305b223efd10fc
Dylan-Liew <dylanliew0503@gmail.com>
2026-03-24 22:50:16 +0800

    Add masking utility test coverage

---
+++ src/test/java/cms/commons/util/MaskingUtilTest.java
@@ -0,0 +1,35 @@
+package cms.commons.util;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+import org.junit.jupiter.api.Test;
+
+public class MaskingUtilTest {
+
+    @Test
+    public void maskPhone() {
+        assertNull(MaskingUtil.maskPhone(null));
+        assertEquals("", MaskingUtil.maskPhone(""));
+        assertEquals("***", MaskingUtil.maskPhone("123"));
+        assertEquals("****", MaskingUtil.maskPhone("1234"));
+        assertEquals("****5678", MaskingUtil.maskPhone("12345678"));
+    }
+
+    @Test
+    public void maskEmail() {
+        assertNull(MaskingUtil.maskEmail(null));
+        assertEquals("", MaskingUtil.maskEmail(""));
+        assertEquals("abc@u.nus.edu", MaskingUtil.maskEmail("abc@u.nus.edu"));
+        assertEquals("abc****@u.nus.edu", MaskingUtil.maskEmail("abcdefg@u.nus.edu"));
+        assertEquals("abc***", MaskingUtil.maskEmail("abcdef"));
+    }
+
+    @Test
+    public void maskUsername() {
+        assertNull(MaskingUtil.maskUsername(null));
+        assertEquals("", MaskingUtil.maskUsername(""));
+        assertEquals("abc", MaskingUtil.maskUsername("abc"));
+        assertEquals("abc****", MaskingUtil.maskUsername("abcdefg"));
+    }
+}
